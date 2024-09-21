eef944aea678548239a957493f9a0cc14cc1ee58
Sopnadevi <jsopnacse@gmail.com>
2024-09-21 01:56:58 -0700

    Day1

---
+++ assignment/CheckNumberIsPositive.java
@@ -0,0 +1,25 @@
+package week1.day1.assignment;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number =5;
+		 
+	     if (number > 0) {
+	 
+	      System.out.println(number + " is positive.");
+	 
+	    } else if (number < 0) {
+	 
+	      System.out.println(number + " is negative.");
+	 
+	    } else {
+	 
+	      System.out.println(number + " is zero.");
+	 
+	    }
+	 
+	}
+
+}
---
+++ assignment/IsPrime.java
@@ -0,0 +1,29 @@
+package week1.day1.assignment;
+
+public class IsPrime {
+
+	
+		 public static void main(String[] args) {
+		      int num = 89;
+		      System.out.println("The given number is: " + num);
+		      // initial count of factors
+		      int count = 0;
+		      // to check prime number
+		      if(num == 2) {
+		         System.out.println(num + " is a prime number");
+		      } else {
+		         // checking number of factors
+		         for(int i = 1; i <= num; i++) {
+		            if(num % i == 0) {
+		               count++;
+		            }
+		         }
+		         // checking number of counts to print result
+		         if(count == 2) {
+		            System.out.println(num + " is a prime number");
+		         } else {
+		            System.out.println(num + " is not a prime number");
+		         }
+		      }
+		   }
+		}
